9840de07b21fb00b8da6456696ab860ee41d59fc
sadisticghostie <168205235+sadisticghostie@users.noreply.github.com>
2024-04-26 23:30:57 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+function startQuestions() {
+    document.getElementById("questions").style.display = "block";
+  }
+  
+  function answerYes() {
+    document.getElementById("questions").style.display = "none";
+      document.getElementById("que").style.display = "none";
+      document.getElementById("init").innerText = "YIPPEE YIPPEE😘😘😘";
+    document.getElementById("message").style.display = "block";
+    document.getElementById("messageText").innerText = "";
+    showConfetti()
+}
+  
+  function answerNo() {
+    document.getElementById("questions").style.display = "none";
+    document.getElementById("que").style.display = "none";
+    document.getElementById("init").innerText = "I'll make you mine👿👿👿 ";
+    document.getElementById("message").style.display = "";
+    document.getElementById("messageText").innerText = "";
+    showConfetti()
+
+}
+  
+// Function to show confetti
+function showConfetti() {
+    const confettiSettings = {
+      target: 'confetti-canvas',
+      respawn: true,
+      colors: ['#ff0000', '#00ff00', '#0000ff'], 
+    };
+  
+    const confetti = new ConfettiGenerator(confettiSettings);
+    confetti.render();
+    setTimeout(() => {
+      confetti.clear();
+    }, 5000);
+  }
+  
